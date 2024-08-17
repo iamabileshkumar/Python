248720088c153210bbef64a2777d0296cc126e96
iamabileshkumar <abileshkumar06@gmail.com>
2024-08-17 16:41:06 +0530

    New Folder Added: Problems on Misc

---
+++ Misc/MatchCase.py
@@ -0,0 +1,8 @@
+num=int(input("Enter any Number:"))
+match num:
+    case 1:
+        print("hello Oneeee!")
+    case 2:
+        print("Hello Twooooooo!")
+    case _:
+        print('Unknown Number')
