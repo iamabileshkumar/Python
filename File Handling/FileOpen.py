a1d685ec28217f06a1b2248b0d192e316dfe2dd9
Abilesh Kumar <84141313+iamabileshkumar@users.noreply.github.com>
2024-08-21 22:05:38 +0530

    FIx : Update FileOpen.py

--- File Handling/FileOpen.py
+++ File Handling/FileOpen.py
@@ -1,4 +1,4 @@
-fileptr=open("C:\Abi\Education\Data Analysis\Python\Jupyter Notebook\Practise_questions\FileHandling\hello.txt",'a+')
+fileptr=open("Practise_questions\FileHandling\hello.txt",'a+')
 if fileptr:
     print("Open SuccessFully")
 try:
