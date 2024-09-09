6c89cecea85777c03927cee3d317c7f2e201fddd
iamabileshkumar <abileshkumar06@gmail.com>
2024-09-09 20:16:26 +0530

    Modified the Code

--- Strings/MatchingCharacterCount.py
+++ Strings/MatchingCharacterCount.py
@@ -1,9 +1,9 @@
 str1 = 'aabcddekll12@'
 str2 = 'bb2211@55k'
 str4=[]
-str1=set(str1)
-str2=set(str2)
-str3=len(str1.intersection(str2))
+str11=set(str1)
+str22=set(str2)
+str3=len(str11.intersection(str22))
 print(f'Count of Matching Character by method 1 is {str3}')
 #Method 2
 for i in str1:
